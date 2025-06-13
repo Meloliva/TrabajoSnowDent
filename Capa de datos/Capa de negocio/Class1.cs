a5a245f32e2724f14473e73a74111d827bbe26af
Meloliva <mel.olivac@gmail.com>
2025-06-13 09:26:55 -0500

    Agregación de todas las capas

---
+++ Capa de datos/Capa de negocio/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_de_datos;
+
+namespace Capa_de_negocio
+{
+    public class Class1
+    {
+    }
+}
